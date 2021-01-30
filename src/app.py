4e873162710e059089e8d356e43a06d635338990
Amalearner <udohosarumwensecynthia@gmail.com>
2021-01-30 17:32:25 +0100

    my first solution

--- src/app.py
+++ src/app.py
@@ -5,6 +5,18 @@ import os
 def palindrome(s):
     # your code goes here
 
+    is_palindrome = False
+    word = s.lower()
+    for i in range(len(word)):
+        if word[i] == word[-1 -i]:
+            is_palindrome = True
+        else:
+            is_palindrome = False
+            break
+    if is_palindrome:
+        return s + " is a palindrome"
+    return  s + " is not a palindrome"
+
 def solution(s):
     return palindrome(s)
 
